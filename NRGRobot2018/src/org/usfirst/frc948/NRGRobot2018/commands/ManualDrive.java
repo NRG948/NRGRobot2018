c835910b6e13c3c4cb4b1e2d56f81da9d8999541
Admin <Admin@10.9.48.166>
2018-01-22 20:51:35 -0800

    Added simple max velocity change algorithm to prevent belt/gear slipping when changing direction quickly Created utilities package and PreferenceKeys Class

--- NRGRobot2018/src/org/usfirst/frc948/NRGRobot2018/commands/ManualDrive.java
+++ NRGRobot2018/src/org/usfirst/frc948/NRGRobot2018/commands/ManualDrive.java
@@ -57,6 +57,7 @@ public class ManualDrive extends Command {
     // Called once after isFinished returns true
     @Override
     protected void end() {
+    	Robot.drive.stop();
     }
 
     // Called when another command which requires one or more of the same
---
+++ NRGRobot2018/src/org/usfirst/frc948/NRGRobot2018/utilities/PreferenceKeys.java
@@ -0,0 +1,5 @@
+package org.usfirst.frc948.NRGRobot2018.utilities;
+
+public class PreferenceKeys {
+	public static final String MAX_VEL_CHANGE = "Max_Velocity_Change";
+}
