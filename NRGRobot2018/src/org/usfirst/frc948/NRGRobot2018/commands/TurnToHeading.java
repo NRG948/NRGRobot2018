47adf4d9e598faf8db22f57a5d8302cc4f2179ab
Ashley Mead <32044217+ashley003@users.noreply.github.com>
2018-01-27 13:52:50 -0800

    Issue #6: Implement TurnToHeading (#14)
    * Initial Commit
    
    Created drivePIDController in drive subsystem (source: ContinuousGyro),
    wrote basic turnToHeadingPID methods.
    Created ContinuousGyro container class for the navx board, which returns
    the current heading (angle) of the robot without wrapping the value to
    -180 degrees from +180 degrees. Used by the drivePIDController.
    Instantiated navX board and ContinuousGyro class.
    
    * minor edits
    
    Trying to clean up my project configuration, so I need to commit these
    changes before deleting and recloning this project
    
    * Add TurnToHeading Command
    
    * Initialize Default Preferences
    
    * Add TurnToHeading -90, Fix PID Init to Read I and D Terms

--- NRGRobot2018/src/org/usfirst/frc948/NRGRobot2018/commands/TurnToHeading.java
+++ NRGRobot2018/src/org/usfirst/frc948/NRGRobot2018/commands/TurnToHeading.java
@@ -1,42 +1,46 @@
 package org.usfirst.frc948.NRGRobot2018.commands;
 
 import org.usfirst.frc948.NRGRobot2018.Robot;
-import org.usfirst.frc948.NRGRobot2018.RobotMap;
 
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
  *
  */
-public class ResetSensors extends Command {
+public class TurnToHeading extends Command {
 
-    public ResetSensors() {
+	double targetHeading;
+	
+    public TurnToHeading(double targetHeading) {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
+    	this.targetHeading = targetHeading;
     	requires(Robot.drive);
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
-    	
+    	Robot.drive.turnToHeadingPIDInit(targetHeading, 1.0);
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	RobotMap.navx.reset();
+    	Robot.drive.turnToHeadingPIDExecute(targetHeading);
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-		return Math.abs(RobotMap.navx.getAngle()) < 0.5;
+        return Robot.drive.onTarget();
     }
 
     // Called once after isFinished returns true
     protected void end() {
+    	Robot.drive.turnToHeadingPIDEnd();
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+    	end();
     }
 }
--- NRGRobot2018/src/org/usfirst/frc948/NRGRobot2018/utilities/PreferenceKeys.java
+++ NRGRobot2018/src/org/usfirst/frc948/NRGRobot2018/utilities/PreferenceKeys.java
@@ -1,5 +1,12 @@
 package org.usfirst.frc948.NRGRobot2018.utilities;
 
 public class PreferenceKeys {
+	
+	public static final String WRITE_DEFAULT = "Write_Default";
+	
 	public static final String MAX_VEL_CHANGE = "Max_Velocity_Change";
+	
+	public static final String TURN_P_TERM = "Turn_P";
+	public static final String TURN_I_TERM = "Turn_I";
+	public static final String TURN_D_TERM = "Turn_D";
 }
