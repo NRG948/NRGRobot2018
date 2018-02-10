edd5cdce405154d2d6a1d9a1f1060a35c4f3615c
Something Something Chu Chu Bar <chuwei0807@gmail.com>
2018-02-10 11:01:14 -0800

    Added default commands for cube acquirer and cube lifter

---
+++ NRGRobot2018/src/org/usfirst/frc948/NRGRobot2018/commands/ManualCubeLift.java
@@ -0,0 +1,42 @@
+package org.usfirst.frc948.NRGRobot2018.commands;
+
+import org.usfirst.frc948.NRGRobot2018.OI;
+import org.usfirst.frc948.NRGRobot2018.Robot;
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class ManualCubeLift extends Command {
+	private final double LIFT_POWER_SCALE_UP = 1;
+	private final double LIFT_POWER_SCALE_DOWN = 0.3;
+
+    public ManualCubeLift() {
+    	requires(Robot.cubelifter);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	double upSpeed = OI.getTriggerL() * LIFT_POWER_SCALE_UP;
+    	double downSpeed = OI.getTriggerR() * LIFT_POWER_SCALE_DOWN;
+    	 
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
