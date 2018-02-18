716aa2a6e6c9de541cb9975f93f8f60b763da36f
SourishMurthy1 <murthysourish31@gmail.com>
2018-02-18 10:58:14 -0800

    Minor changes to DriveToXY

--- NRGRobot2018/src/org/usfirst/frc948/NRGRobot2018/commands/DriveToXY.java
+++ NRGRobot2018/src/org/usfirst/frc948/NRGRobot2018/commands/DriveToXY.java
@@ -13,44 +13,47 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  */
 public class DriveToXY extends Command {
 
-    public DriveToXY() {
-        requires(Robot.drive);
-    }
+	public DriveToXY() {
+		requires(Robot.drive);
+	}
 
-    // Called just before this Command runs the first time
-    protected void initialize() {
-    }
+	// Called just before this Command runs the first time
+	protected void initialize() {
+	}
 
-    // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
+	// Called repeatedly when this Command is scheduled to run
+	protected void execute() {
+		double currentX = Robot.positionTracker.getX();
+		double currentY = Robot.positionTracker.getY();
+		
+		double desiredX = Robot.preferences.getDouble(PreferenceKeys.X2, 0.0);
+		double desiredY = Robot.preferences.getDouble(PreferenceKeys.Y2, 0.0);
+		double desiredHeading = Math.atan2(desiredX - currentX, desiredY - currentY) * 180 / Math.PI;
+		double distanceToTravel = Math
+				.sqrt((desiredX - currentX) * (desiredX - currentX) + (desiredY - currentY) * (desiredY - currentY));
 
-		double x2 = Robot.preferences.getDouble(PreferenceKeys.X2, 0.0);
-		double y2 = Robot.preferences.getDouble(PreferenceKeys.Y2, 0.0);
-		double x1 = Robot.positionTracker.getX();
-		double y1 = Robot.positionTracker.getY();
-		double theta = Math.atan2(x2 - x1, y2 - y1) * 180 / Math.PI;
-		double distance = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
-		SmartDashboard.putString("DriveToXY: heading, distance", String.format("%.2f, %.2f", theta, distance));
+		SmartDashboard.putString("DriveToXY: heading, distance",
+				String.format("%.2f, %.2f", desiredHeading, distanceToTravel));
+
 		new CommandGroup() {
 			{
-				addSequential(new TurnToHeading(theta));
-				addSequential(new DriveStraightDistance(0.5, distance, Drive.Direction.FORWARD));
+				addSequential(new TurnToHeading(desiredHeading));
+				addSequential(new DriveStraightDistance(0.5, distanceToTravel, Drive.Direction.FORWARD));
 			}
 		}.start();
-	
-    }
+	}
 
-    // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() {
-    	return true;
-    }
+	// Make this return true when this Command no longer needs to run execute()
+	protected boolean isFinished() {
+		return true;
+	}
 
-    // Called once after isFinished returns true
-    protected void end() {
-    }
+	// Called once after isFinished returns true
+	protected void end() {
+	}
 
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    protected void interrupted() {
-    }
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	protected void interrupted() {
+	}
 }
