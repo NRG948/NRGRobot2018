b9c457706d82339a0fec53eaa1eefe7233f6e91e
ValliappanValliappan <gtvalliappan@gmail.com>
2018-02-24 09:51:48 -0800

    Wrote EjectUntilCubeOut command to eject a cube.

--- NRGRobot2018/src/org/usfirst/frc948/NRGRobot2018/commands/EjectUntilCubeOut.java
+++ NRGRobot2018/src/org/usfirst/frc948/NRGRobot2018/commands/EjectUntilCubeOut.java
@@ -1,5 +1,7 @@
 package org.usfirst.frc948.NRGRobot2018.commands;
 
+import edu.wpi.first.wpilibj.Timer;
+
 import org.usfirst.frc948.NRGRobot2018.Robot;
 import org.usfirst.frc948.NRGRobot2018.subsystems.CubeAcquirer.Direction;
 
@@ -8,13 +10,17 @@ import edu.wpi.first.wpilibj.command.Command;
 /**
  * AcquireUntilCubeDetected: Run acquirer wheels inward until cube sensor activated.
  */
-public class AcquireUntilCubeDetected extends Command {
-	private final double power;
+public class EjectUntilCubeOut extends Command {
+	private final double power, delay; // Delay (seconds) is to make sure cube has been completely ejected
+	private final Timer timer;
 
-	public AcquireUntilCubeDetected(double power) {
+	public EjectUntilCubeOut(double power, double delay) {
 		requires(Robot.cubeAcquirer);
 		
 		this.power = power;
+		this.delay = delay;
+		
+		timer = new Timer();
 	}
 
 	// Called just before this Command runs the first time
@@ -23,12 +29,12 @@ public class AcquireUntilCubeDetected extends Command {
 
 	// Called repeatedly when this Command is scheduled to run
 	protected void execute() {
-		Robot.cubeAcquirer.acquire(power, Direction.ACQUIRE);
+		Robot.cubeAcquirer.acquire(power, Direction.EJECT);
 	}
 
 	// Make this return true when this Command no longer needs to run execute()
 	protected boolean isFinished() {
-		return Robot.cubeAcquirer.isCubeIn();
+		return !Robot.cubeAcquirer.isCubeIn() && timer.hasPeriodPassed(delay);
 	}
 
 	// Called once after isFinished returns true
