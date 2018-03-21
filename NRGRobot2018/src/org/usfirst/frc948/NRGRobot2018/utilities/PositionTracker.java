1e91d6fc0ae038516d075c3dde93aa78ad3a3855
Nipun Chhajed <nipunchhajed@gmail.com>
2018-03-20 20:34:01 -0700

    Add references to omni coordinates and mechanum coordinates separately

--- NRGRobot2018/src/org/usfirst/frc948/NRGRobot2018/utilities/PositionTracker.java
+++ NRGRobot2018/src/org/usfirst/frc948/NRGRobot2018/utilities/PositionTracker.java
@@ -4,14 +4,18 @@ import org.usfirst.frc948.NRGRobot2018.RobotMap;
 
 import com.sun.glass.ui.Robot;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 public class PositionTracker {
-	private double x;
-	private double y;
+	private double omniX;
+	private double omniY;
 	private double xGoal;
 	private double yGoal;
 	private double prevXEncoder;
-	private double prevYEncoder;
+	private double prevYEncoder; 
 	
+	private double mechX;
+	private double mechY;
 	/*private double prevLeftFrontEncoder;
 	private double prevLeftRearEncoder;
 	private double prevRightFrontEncoder;
@@ -24,12 +28,12 @@ public class PositionTracker {
 	}
 
 	public void updatePosition() {
-		boolean usingFourEncoders = org.usfirst.frc948.NRGRobot2018.Robot.preferences.getBoolean(PreferenceKeys.USE_FOUR_ENCODERS, false);
-		if(usingFourEncoders) {
-			updatePositionFourEncoders();
-		} else {
-			updatePositionTwoEncoders();
-		}
+		updatePositionFourEncoders();
+		updatePositionTwoEncoders();
+		SmartDashboard.putNumber("Position Tracker/mechX", mechX);
+		SmartDashboard.putNumber("Position Tracker/mechY", mechY);
+		SmartDashboard.putNumber("Position Tracker/omniX", omniX);
+		SmartDashboard.putNumber("Position Tracker/omniY", omniY);
 	}
 	
 	public void updatePositionTwoEncoders() {
@@ -44,8 +48,8 @@ public class PositionTracker {
 
 		// converting to field reference frame
 		double robotToFieldRadians = Math.toRadians(90 - (currHeading + Math.toDegrees(Math.atan2(xDelta, yDelta))));
-		x += distanceTraveled * Math.cos(robotToFieldRadians);
-		y += distanceTraveled * Math.sin(robotToFieldRadians);
+		omniX += distanceTraveled * Math.cos(robotToFieldRadians);
+		omniY += distanceTraveled * Math.sin(robotToFieldRadians);
 
 		prevXEncoder = currXEncoder;
 		prevYEncoder = currYEncoder;
@@ -60,21 +64,31 @@ public class PositionTracker {
 		double xPos = ((currLeftFrontEncoder + currRightRearEncoder) - (currRightFrontEncoder + currLeftRearEncoder)) / 4.0;
 		double yPos = (currLeftFrontEncoder + currLeftRearEncoder + currRightFrontEncoder + currRightRearEncoder) / 4.0;
 		
-		this.x = xPos;
-		this.y = yPos;
+		this.omniX = xPos;
+		this.omniY = yPos;
 	}
 	
 	public double getX() {
-		return x;
+		boolean usingFourEncoders = org.usfirst.frc948.NRGRobot2018.Robot.preferences.getBoolean(PreferenceKeys.USE_FOUR_ENCODERS, false);
+		if(usingFourEncoders) {
+			return mechX;
+		}
+		return omniX;
 	}
 
 	public double getY() {
-		return y;
+		boolean usingFourEncoders = org.usfirst.frc948.NRGRobot2018.Robot.preferences.getBoolean(PreferenceKeys.USE_FOUR_ENCODERS, false);
+		if(usingFourEncoders) {
+			return mechY;
+		}
+		return omniY;
 	}
 
 	public void setXY(double x, double y) {
-		this.x = x;
-		this.y = y;
+		this.mechX = x;
+		this.mechY = y;
+		this.omniX = x;
+		this.omniY = y;
 	}
 	
 	public double getXGoal() {
@@ -101,6 +115,6 @@ public class PositionTracker {
 
 	@Override
 	public String toString() {
-		return String.format("Position: (%.1f, %.1f)", x, y);
+		return String.format("Position: (%.1f, %.1f)", omniX, omniY);
 	}
 }
