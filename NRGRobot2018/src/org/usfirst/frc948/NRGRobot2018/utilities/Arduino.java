e6f231c26f8b101dfebf70f3f60f2a568e2e5ac7
NRG Driver Station <chuwei0807@gmail.com>
2018-03-22 19:25:16 -0700

    disco lights

--- NRGRobot2018/src/org/usfirst/frc948/NRGRobot2018/utilities/Arduino.java
+++ NRGRobot2018/src/org/usfirst/frc948/NRGRobot2018/utilities/Arduino.java
@@ -3,14 +3,17 @@ package org.usfirst.frc948.NRGRobot2018.utilities;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import org.usfirst.frc948.NRGRobot2018.OI;
 import org.usfirst.frc948.NRGRobot2018.Robot;
 import org.usfirst.frc948.NRGRobot2018.vision.IPixyLink;
 
 public class Arduino {
 	private IPixyLink link;
+	private boolean prevClimberState;
 
 	public Arduino(IPixyLink link) {
 		this.link = link;
+		prevClimberState = false;
 	}
 
 	public void startArduinoThread() { // starts vision thread
@@ -20,14 +23,16 @@ public class Arduino {
 	void updateLights() {
 		byte[] outBuf = new byte[1];
 		boolean currentCubeSwitchState = Robot.cubeAcquirer.isCubeIn();
+		boolean currClimberState = OI.climberButton.get();
 
 		if (currentCubeSwitchState != Robot.cubeAcquirer.lastCubeSwitchState) {
 			// cube is fully acquired if switch state is true
 			outBuf[0] = (byte) (currentCubeSwitchState ? ArduinoLightConstants.CUBE_SENSOR_PRESSED
 					: ArduinoLightConstants.CUBE_SENSOR_RELEASED);
 			Robot.cubeAcquirer.lastCubeSwitchState = currentCubeSwitchState;
-		} else {
-			
+		} else if (currClimberState != prevClimberState){
+			outBuf[0] = (byte) (ArduinoLightConstants.CLIMBER_BUTTON_PRESSED);
+			prevClimberState = currClimberState;
 		}
 
 		link.send(outBuf);
