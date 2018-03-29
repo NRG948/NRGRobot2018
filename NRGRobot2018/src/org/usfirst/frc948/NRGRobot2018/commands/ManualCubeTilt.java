86c41b119f9f3116372ba691e387c4a4a189a2fd
AnushGarimella <s-garimellaan@bsd405.org>
2018-03-28 18:40:41 -0700

    Re-enabled tilter PID

--- NRGRobot2018/src/org/usfirst/frc948/NRGRobot2018/commands/ManualCubeTilt.java
+++ NRGRobot2018/src/org/usfirst/frc948/NRGRobot2018/commands/ManualCubeTilt.java
@@ -34,15 +34,15 @@ public class ManualCubeTilt extends Command {
 			power = CubeTilter.TILT_DOWN_POWER;
 			Robot.cubeTilter.rawTilt(power);
 		} else {
-//			double encoderValue = RobotMap.cubeTiltEncoder.getDistance();
-//
-//			if (encoderValue >= CubeTilter.TILTER_PID_CUTOFF) {
-//				if (prevPower != 0) {
-//					Robot.cubeTilter.tiltToAnglePIDIntialize(encoderValue, 50);
-//					System.out.println("Entering Tilter PID mode for setpoint " + encoderValue);
-//				}
-//				Robot.cubeTilter.tiltToAnglePIDExecute();
-//			}
+			double currAngle = RobotMap.cubeTiltEncoder.getDistance();
+
+			if (currAngle >= CubeTilter.TILTER_PID_CUTOFF) {
+				if (prevPower != 0) {
+					Robot.cubeTilter.tiltToAnglePIDIntialize(currAngle, 1);
+					System.out.println("Entering Tilter PID mode for setpoint " + currAngle);
+				}
+				Robot.cubeTilter.tiltToAnglePIDExecute();
+			}
 		}
 
 		prevPower = power;
--- NRGRobot2018/src/org/usfirst/frc948/NRGRobot2018/subsystems/CubeTilter.java
+++ NRGRobot2018/src/org/usfirst/frc948/NRGRobot2018/subsystems/CubeTilter.java
@@ -23,16 +23,16 @@ public class CubeTilter extends Subsystem {
 	public static final double DEFAULT_TILT_D = 0;
 	
 	public static final double TILTER_UP = 0;
-	public static final double TILTER_DOWN = -1200;
+	public static final double TILTER_DOWN = -45;
 	
-	public static final double TILTER_RANGE_MIN = -2300;
+	public static final double TILTER_RANGE_MIN = -90;
 	public static final double TILTER_RANGE_MAX = 0;
-	public static final double TILTER_PID_CUTOFF = -2000;
+	public static final double TILTER_PID_CUTOFF = -80;
 	
 	private static SimplePIDController tiltPIDController;
 
     public void initDefaultCommand() {
-        setDefaultCommand(new ManualCubeTilt());
+//        setDefaultCommand(new ManualCubeTilt());
     }
     
     private void createTiltPIDController(double setpoint, double tolerance){
